8cc60fe22946b4f144e07941adc28e310bcdf326
Hitesh Thaliya <hiteshthaliya123@gmail.com>
2026-06-13 00:30:56 +0530

    solve using one loop

--- 07_factorial_1_to_n.py
+++ 07_factorial_1_to_n.py
@@ -1,6 +1,5 @@
 # Calculate and print the factorial of every number from 1 to 100.
-for num in range(1,11):
-    fact = 1
-    for i in range(1,num+1):
-        fact *=i
-    print(f"{num}! = {fact}")    
+fact =1
+for i in range(1,11):
+    fact *=i
+    print(f"{i}! = {fact}")    
